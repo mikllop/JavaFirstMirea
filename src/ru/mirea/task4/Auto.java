d41a769a12a61ef5a9fb31b6bc10d78c2b67e3c7
Mihail Lopatkov <113026841+mikllop@users.noreply.github.com>
2022-10-19 14:26:10 +0300

    Create Auto.java

---
+++ src/ru/mirea/task4/Auto.java
@@ -0,0 +1,19 @@
+package ru.mirea.task4;
+
+public class Auto extends Vehicle {
+    private double time;
+    private double price;
+
+    public Auto(double time, double price) {
+        this.time = time;
+        this.price = price;
+    }
+
+    public void findPrice(){
+        System.out.println(price * 2);
+    }
+
+    public void findTime(){
+        System.out.println(time * 8);
+    }
+}
