2a996ade079ccbe0c4d8b247f46abf07fab9e996
Mihail Lopatkov <113026841+mikllop@users.noreply.github.com>
2022-10-19 14:47:20 +0300

    Create Ship.java

---
+++ src/ru/mirea/task4/Ship.java
@@ -0,0 +1,19 @@
+package ru.mirea.task4;
+
+public class Ship extends Vehicle {
+    private double time;
+    private double price;
+
+    public Ship(double time, double price) {
+        this.time = time;
+        this.price = price;
+    }
+
+    public void findPrice(){
+        System.out.println(price * 3);
+    }
+
+    public void findTime(){
+        System.out.println(time * 6);
+    }
+}
