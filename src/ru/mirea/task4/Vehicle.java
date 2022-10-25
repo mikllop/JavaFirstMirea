fd44ac26ac9282849841540f3a9575cdf46e1921
Mihail Lopatkov <113026841+mikllop@users.noreply.github.com>
2022-10-25 20:24:16 +0300

    Create Vehicle.java

---
+++ src/ru/mirea/task4/Vehicle.java
@@ -0,0 +1,22 @@
+package ru.mirea.task4;
+
+public abstract class Vehicle {
+    private double time;
+    private double price;
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void findPrice(){
+
+    }
+
+    public void findTime(){
+
+    }
+}
