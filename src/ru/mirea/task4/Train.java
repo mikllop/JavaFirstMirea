e3d4b2bdcd9ec7f54e5cec7fc7ff291abb7d392f
Mihail Lopatkov <113026841+mikllop@users.noreply.github.com>
2022-10-25 20:23:51 +0300

    Create Train.java

---
+++ src/ru/mirea/task4/Train.java
@@ -0,0 +1,19 @@
+package ru.mirea.task4;
+
+public class Train extends Vehicle {
+    private double time;
+    private double price;
+
+    public Train(double time, double price) {
+        this.time = time;
+        this.price = price;
+    }
+
+    public void findPrice(){
+        System.out.println(price * 1.5);
+    }
+
+    public void findTime(){
+        System.out.println(time * 10);
+    }
+}
