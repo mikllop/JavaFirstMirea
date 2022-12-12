f0b6f14e05e22fde960f746a3ad7cd9e25530cf6
Mihail Lopatkov <113026841+mikllop@users.noreply.github.com>
2022-12-12 22:14:00 +0300

    Create Execute.java

---
+++ src/ru/mirea/task22/Execute.java
@@ -0,0 +1,8 @@
+private double Execute(char op, double first, double second) => op switch {
+    '+' => first + second,					//	Сложение
+    '-' => first - second,					//	Вычитание
+    '*' => first * second,					//	Умножение
+    '/' => first / second,					//	Деление
+    '^' => Math.Pow(first, second),	//	Степень
+    _ => 0	//	Возвращает, если не был найден подходящий оператор
+};
