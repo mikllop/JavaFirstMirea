d1d9db45ce78b10f285ffc5da61b4b2099b7fc13
Mihail Lopatkov <113026841+mikllop@users.noreply.github.com>
2022-12-12 22:15:42 +0300

    Create GetStringNumber.java

---
+++ src/ru/mirea/task22/GetStringNumber.java
@@ -0,0 +1,16 @@
+private string GetStringNumber(string expr, ref int pos)
+{
+  string strNumber = "";
+  for (; pos < expr.Length; pos++)
+  {
+    char num = expr[pos];
+    if (Char.IsDigit(num))
+      strNumber += num;
+    else
+    {
+      pos--;
+      break;
+    }
+  }
+  return strNumber;
+}
